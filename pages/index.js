c292d8d4b506d568083c37016e788224b08c936e
tnikhilkumar <nikhilkumarnikky17@gmail.com>
2023-05-06 17:31:09 +0530

    warnings-removed

--- pages/index.js
+++ pages/index.js
@@ -15,13 +15,13 @@ export default function Home() {
        <h1 className='text-4xl font-bold'>Blog Posts, t_nikhilkumar</h1>
        {dataBlog.map((post) => (
         <Link href={`/blog/${post.slug}`} passHref>
-  <div className='block my-8 border border-white p-4 rounded-lg hover:bg-white hover:text-black '>
-    <h2 className='text-2xl font-bold'>{post.title}</h2>
-    <p className='mt-2'>
-      {post.content.substring(0, 32)}...
-    </p>
-  </div>
-</Link> 
+           <div className='block my-8 border border-white p-4 rounded-lg hover:bg-white hover:text-black '>
+          <h2 className='text-2xl font-bold'>{post.title}</h2>
+         <p className='mt-2'>
+          {post.content.substring(0, 32)}...
+         </p>
+         </div>
+       </Link> 
         ))}
       </main>
     </div>
